6025cdf67463b3f26b4632a76e383c45f11da18d
TrungToan01 <toanb1805824@student.ctu.edu.vn>
2022-02-26 19:24:15 +0700

    create signup

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 const App = () => {
   return (
     <div className="containers">
-      <Login></Login>
+      <SignUp></SignUp>
+      {/* <Login></Login> */}
       {/* <Home></Home> */}
     </div>
   );
