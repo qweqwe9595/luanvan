d290678f65ac6f9319357b132472ca5696a7167d
qweqwe9595 <73086329+qweqwe9595@users.noreply.github.com>
2022-02-26 19:28:39 +0700

    temp

--- fe/src/App.js
+++ fe/src/App.js
@@ -4,13 +4,8 @@ import Login from "./pages/login/Login";
 const App = () => {
   return (
     <div className="containers">
-<<<<<<< HEAD
-      {/* <Login></Login> */}
-      <Home></Home>
-=======
       {/*<Login></Login> */}
        <Home></Home> 
->>>>>>> 5be2e79fe777e40dbb2a1b8ee448df856cd466b6
     </div>
   );
 };
