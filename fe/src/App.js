d57d800ff62182d73661d3171d9fcb7b8acaed4a
TrungToan01 <toanb1805824@student.ctu.edu.vn>
2022-02-27 09:12:24 +0700

    test1

--- fe/src/App.js
+++ fe/src/App.js
@@ -5,19 +5,16 @@ import SignUp from "./pages/signup/SignUp";
 const App = () => {
   return (
     <div className="containers">
-<<<<<<< HEAD
+
       <SignUp></SignUp>
       {/* <Login></Login> */}
       {/* <Home></Home> */}
-=======
-<<<<<<< HEAD
       {/* <Login></Login> */}
-      <Home></Home>
-=======
+      {/* <Home></Home> */}
+
       {/*<Login></Login> */}
-       <Home></Home> 
->>>>>>> 5be2e79fe777e40dbb2a1b8ee448df856cd466b6
->>>>>>> 633d3994493d89c4b50a74b3ce59acb785f668b4
+       {/* <Home></Home>  */}
+
     </div>
   );
 };
