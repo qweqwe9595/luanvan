82aa9256be92656753820b5cfa099fc3b811e481
Đinh Vĩnh Thái <dinhvinhthai.dev@gmail.com>
2022-02-27 19:53:29 +0700

    Update post by Thai

--- fe/src/component/share/Share.js
+++ fe/src/component/share/Share.js
@@ -6,17 +6,9 @@ import Modal from 'react-modal';
 import axios from "axios";
 
 function Share() {
-    const [modalIsOpen,setModalIsOpen] = useState(false);
-    const setModalIsOpenToTrue =()=>{
-        setModalIsOpen(true)
-    }
-    const setModalIsOpenToFalse =()=>{
-        setModalIsOpen(false)
-    }
-
-    const [desc, setDesc] = useState("");
+       const [desc, setDesc] = useState("");
   
-    const Post = () => {
+    const Share = () => {
       axios
       .post("http://localhost:5000/api/posts", {
         desc,
@@ -30,51 +22,20 @@ function Share() {
     };
 
   return (
-    <>
-    <div className="share" onClick={setModalIsOpenToTrue}>
+    <div className="share" >
       <img
         src="https://genk.mediacdn.vn/139269124445442048/2021/3/13/ava-1615607562651629147713.jpeg"
         alt=""
       />
-      <input type="text" placeholder="Viết gì đó..." readOnly/>
+      <input type="text" placeholder="Viết gì đó..." value={desc} onChange={(e) => {setDesc(e.target.value); }}/>
       <div className="share-right">
         <FaPhotoVideo className="share-img"></FaPhotoVideo>
-        <div className="share-btn">
+        <div className="share-btn" onClick={() => {Share();}}>
           <FaRegPaperPlane />
           <span>POST!</span>
         </div>
       </div>
-    </div>
-    <div className="share-modal">
-    <Modal isOpen={modalIsOpen} ariaHideApp={false} style={{
-            overlay: {
-              position: 'fixed',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            },
-            content: {
-              background: 'white',
-              width: '500px',
-              height: '400px',
-              position: 'relative',
-              border: '1px solid #000',
-              borderRadius: '0.3rem',
-            }}}>
-              <div className="share-modal-post">
-              <p>Tạo bài viết mới</p>
-                <form>
-                  <textarea placeholder="Viết gì đó..." className="share-modal-post-desc" value={desc} onChange={(e) => {setDesc(e.target.value); }}></textarea>
-                  <div>
-                  <button className="share-modal-post-btn1" onClick={setModalIsOpenToFalse}>HỦY</button>
-                  
-                  <button className="share-modal-post-btn2" onClick={() => {Post();}}>ĐĂNG</button>
-                  </div>
-                </form>
-            </div> 
-    </Modal>
-  </div>
-  </>    
+    </div>    
   );
 }
 
