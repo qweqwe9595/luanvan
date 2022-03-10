7e115f1d4d9a172c8fe8bbfd929d91f3fbf75ea2
qweqwe9595 <73086329+qweqwe9595@users.noreply.github.com>
2022-03-10 13:06:54 +0700

    add be seaching by name

--- fe/src/App.js
+++ fe/src/App.js
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <div className="app">
       <Profile></Profile>
-      {/* <Login></Login> */}
-      {/* <SignUp></SignUp> */}
+      <Login></Login>
+      <SignUp></SignUp>
     </div>
   );
 };
