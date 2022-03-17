f80d09e5c3161e7262c043e84f1c6e44f28591bc
qweqwe9595 <73086329+qweqwe9595@users.noreply.github.com>
2022-03-17 12:34:14 +0700

    add unfriend be

--- Be/routes/usersRoute.js
+++ Be/routes/usersRoute.js
@@ -11,6 +11,7 @@ const {
   acceptFriend,
   updateAUser,
   deleteAUser,
+  unFriend,
 } = require("../controller/usersController");
 
 //searching user
@@ -33,6 +34,9 @@ router.patch("/accept/:id", acceptFriend);
 
 //update user
 router.patch("/:id", updateAUser);
+
+//unfriend
+router.patch("/unfriend/:id", unFriend);
 
 //delete user
 router.delete("/:id", deleteAUser);
