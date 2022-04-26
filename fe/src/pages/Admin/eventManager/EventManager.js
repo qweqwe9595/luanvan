02779b2b8f9c3f3717312b865f5ffb58a6ba58e2
ismphi <toanb1805824@student.ctu.edu.vn>
2022-04-26 17:36:37 +0700

    toan da chinh sua

--- fe/src/pages/Admin/eventManager/EventManager.js
+++ fe/src/pages/Admin/eventManager/EventManager.js
@@ -1,4 +1,5 @@
 import StatusCard from "../../../component/admin/StatusCard";
+import "./EventManager.scss";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -94,57 +95,54 @@ function EventManager() {
             ""
           )}
         </div>
-        <div className="event_manager_content mt-10">
+        <div className="event_manager_content">
           <div className="event_manager_content_header">
-            <span className="px-2 py-1 bg-toan rounded-xl font-bold text-white">
-              Danh sách các sự kiện
-            </span>
+            <span>Danh sách các sự kiện</span>
           </div>
-          <div className="grid grid-cols-2 p-4 justify-center lg:grid-cols-3 xl:grid-cols-4 gap-y-14">
-            {eventDetails?.map((eventI, index) => {
-              return (
-                <div className="justify-self-center w-60 h-52" key={index}>
-                  <img
-                    src={`http://localhost:5000/images/${eventI?.img}`}
-                    className="w-full h-40 object-cover"
-                  ></img>
-                  <div className="time">
-                    {eventI?.startTime ? (
-                      <span>
-                        Thời gian diễn ra sự kiện:{" "}
-                        <b>
-                          {new Date(eventI.startTime).toLocaleDateString(
-                            "en-US"
-                          )}
-                        </b>
-                      </span>
+          <div className="center_events">
+          {eventDetails?.map((eventI, index) => {
+            return (
+              <div className="event_tags" key={index}>
+                <img
+                  src={`http://localhost:5000/images/${eventI?.img}`}
+                  className="cover"
+                ></img>
+                <div className="time">
+                  {eventI?.startTime ? (
+                    <span>
+                      Thời gian diễn ra sự kiện:{" "}
+                      <b>
+                        {new Date(eventI.startTime).toLocaleDateString("en-US")}
+                      </b>
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <Link to={`/eventContent/${eventI?._id}`}>
+                  <div className="title">
+                    {eventI?.eventName ? (
+                      <span>{eventI?.eventName}</span>
                     ) : (
-                      ""
+                      "eventName"
                     )}
                   </div>
-                  <Link to={`/eventContent/${eventI?._id}`}>
-                    <div className="title">
-                      {eventI?.eventName ? (
-                        <span>{eventI?.eventName}</span>
-                      ) : (
-                        "eventName"
-                      )}
-                    </div>
-                  </Link>
-                  <div className="button_delete">
-                    <button
-                      onClick={() => {
-                        xoaEvent(eventI._id);
-                      }}
-                    >
-                      Xóa sự kiện
-                    </button>
-                  </div>
+                </Link>
+                <div className="button_delete">
+                  <button
+                    onClick={() => {
+                      xoaEvent(eventI._id);
+                    }}
+                  >
+                    Xóa sự kiện
+                  </button>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
+        </div>
+        
       </div>
       {open ? <CreateNewEvent setOpen={setOpen}></CreateNewEvent> : ""}
     </>
